89bdc5340ea7c8503cfd36a874f03a80b3fe8176
kinghappy24 <91432665+kinghappy24@users.noreply.github.com>
2024-06-26 04:05:04 +0200

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+    Hallo <?php echo $_POST["Name"]; ?> <?php echo $_POST["Nachname"]; ?><br>
+    Vielen Dank für die verbindung mit uns, wir werden uns so schnell wie möglich bei ihnen melden.<br>
+    <br>
+    Sie werden über die Email "<?php echo $_POST["email"]; ?> bald von uns erreicht.
+    <a href="index.html">Zurück<a>
+</body>
+</html>
